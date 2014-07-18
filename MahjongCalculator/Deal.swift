bef66ace228572a433f171e61d684c2238414e9a
Ken Tominaga <ken.for.develop@gmail.com>
2014-07-18 09:58:40 +0900

    Add Deal class

---
+++ MahjongCalculator/Deal.swift
@@ -0,0 +1,13 @@
+//
+//  Deal.swift
+//  MahjongCalculator
+//
+//  Created by Ken Tominaga on 7/17/14.
+//  Copyright (c) 2014 Tommy. All rights reserved.
+//
+
+import UIKit
+
+class Deal: NSObject {
+   
+}
